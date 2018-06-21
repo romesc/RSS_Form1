69df7b68185f4f8c872202bb26282c89baee4912
Rodrigo Mathias <ro_mescua@outlook.com>
2018-06-20 22:23:57 -0300

    Criação da classe UtilModelos

---
+++ RSS_Form1/Classes/UtilModelos.cs
@@ -0,0 +1,10 @@
+﻿namespace RSS_Form1.Modelos
+{
+    public partial class Categoria
+    {
+        public override string ToString()
+        {
+            return cat_descricao.ToString();
+        }
+    }
+}
